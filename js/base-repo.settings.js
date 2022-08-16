817c83db7809a7be6a4b77d71f0b22c074951cca
Thomas Jejkal <thomas.jejkal@kit.edu>
2022-08-16 21:46:12 +0200

    Update base-repo.settings.js

--- js/base-repo.settings.js
+++ js/base-repo.settings.js
@@ -1,9 +1,6 @@
 export let ajaxBaseUrl = "http://localhost:8090/api/v1/";
 export const keycloak = undefined;
     /*Keycloak({
-    //url: 'https://auth.nffa.eu/auth',
-    //realm: 'NEP',
-    //clientId: 'metastore'
     url: 'http://127.0.0.1:8080/auth',
     realm: 'myrealm',
     clientId: 'keycloak-angular'
