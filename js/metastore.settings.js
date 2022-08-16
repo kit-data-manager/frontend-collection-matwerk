51e3eba60013bdf6371149491b757b8982edb227
Thomas Jejkal <thomas.jejkal@kit.edu>
2022-08-16 21:46:27 +0200

    Update metastore.settings.js

--- js/metastore.settings.js
+++ js/metastore.settings.js
@@ -1,9 +1,6 @@
 export let ajaxBaseUrl = "http://mm3.datamanager.kit.edu:8040/api/v1/";
 export const keycloak = undefined;
     /*Keycloak({
-    //url: 'https://auth.nffa.eu/auth',
-    //realm: 'NEP',
-    //clientId: 'metastore'
     url: 'http://127.0.0.1:8080/auth',
     realm: 'myrealm',
     clientId: 'keycloak-angular'
