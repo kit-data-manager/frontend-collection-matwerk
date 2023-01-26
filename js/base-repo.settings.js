2fa59e1a4da8369ceabbafb7a3aaf063e66e2983
Thomas Jejkal <thomas.jejkal@kit.edu>
2023-01-26 21:59:24 +0100

    Adapted frontend settings to demo site

--- js/base-repo.settings.js
+++ js/base-repo.settings.js
@@ -1,16 +1,15 @@
 export let ajaxBaseUrl = "http://localhost:8081/api/v1/";
-export const keycloak = undefined;
-/*export const keycloak = Keycloak({
+export const keycloak = Keycloak({
     url: 'https://gateway.datamanager.kit.edu:8443/',
     realm: 'dem_testing',
     clientId: 'kitdm-services'
-});*/
+});
 export const tags = [{"name":"tag", "color":"red"}, {"name":"tag2", "color":"blue"},{"name":"tag3", "color":"green"}];
-export const showServiceUrl = true;
+export const showServiceUrl = false;
 
 export const appDescription = {
     "app-logo":"./images/disks.jpg",
-    "app-title":"Base-Repo UI",
+    "app-title":"Base-Repo Demonstrator",
     "app-subtitle":"Data Resource Management"
 };
 
--- js/metastore.settings.js
+++ js/metastore.settings.js
@@ -1,11 +1,11 @@
 export let ajaxBaseUrl = "http://localhost:8040/api/v1/";
 export const keycloak = Keycloak({
     url: 'https://gateway.datamanager.kit.edu:8443/',
-    realm: 'nfdi4ing',
+    realm: 'dem_testing',
     clientId: 'kitdm-services'
 });
 
-export const showServiceUrl = true;
+export const showServiceUrl = false;
 
 export const appDescription = {
     "app-logo":"./images/metadata.jpg",
