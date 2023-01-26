db4b0cf5451c3122e5bdd7cb5fc7c37cf9c85740
Thomas Jejkal <thomas.jejkal@kit.edu>
2023-01-26 22:52:24 +0100

    Fixed service URLs

--- js/metastore.settings.js
+++ js/metastore.settings.js
@@ -1,4 +1,4 @@
-export let ajaxBaseUrl = "http://localhost:8040/api/v1/";
+export let ajaxBaseUrl = "https://demo.datamanager.kit.edu:8443/metastore/api/v1/";
 export const keycloak = Keycloak({
     url: 'https://gateway.datamanager.kit.edu:8443/',
     realm: 'dem_testing',
