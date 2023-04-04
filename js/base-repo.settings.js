97cdb5fef523790dc48b6417bcd64e7d5fe39532
Thomas Jejkal <thomas.jejkal@kit.edu>
2023-04-04 15:22:06 +0200

    Started with FDO search

--- js/base-repo.settings.js
+++ js/base-repo.settings.js
@@ -1,10 +1,11 @@
 //export let ajaxBaseUrl = "https://demo.datamanager.kit.edu:8443/base-repo/api/v1/";
 export let ajaxBaseUrl = "http://localhost:8081/api/v1/";
-export const keycloak = Keycloak({
+export const keycloak = undefined;
+/*Keycloak({
     url: 'https://gateway.datamanager.kit.edu:8443/',
     realm: 'dem_testing',
     clientId: 'kitdm-services'
-});
+});*/
 export const tags = [
     {"name":"rawData", "color":"red"},
     {"name":"analyzedData", "color":"green"},
