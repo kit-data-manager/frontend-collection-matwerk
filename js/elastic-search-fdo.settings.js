3472f6d124ad113313fbc880e26a07b3f9086e34
Thomas Jejkal <thomas.jejkal@kit.edu>
2023-04-25 18:32:17 +0200

    Finished evaluation version of FDO search, added aggregations for FDO type, topic and license

--- js/elastic-search-fdo.settings.js
+++ js/elastic-search-fdo.settings.js
@@ -1,18 +1,19 @@
-export let ajaxBaseUrl = "https://demo.datamanager.kit.edu:8443/metastore/api/v1/";
+export let ajaxBaseUrl = "http://localhost:9200/typedpidmaker/_";//https://demo.datamanager.kit.edu:8443/metastore/api/v1/";
 //export let ajaxBaseUrl = "http://localhost:8081/api/v1/";
-export const keycloak = Keycloak({
+export const keycloak = undefined;
+/*Keycloak({
     url: 'https://gateway.datamanager.kit.edu:8443/',
     realm: 'dem_testing',
     clientId: 'kitdm-services'
-});
+});*/
 
 export const showServiceUrl = false;
 export const page_size = 5;
 
 export const appDescription = {
     "app-logo":"./images/search.jpg",
-    "app-title":"MetaStore Search Demonstrator",
-    "app-subtitle":"Metadata Search"
+    "app-title":"FDO Search Demonstrator",
+    "app-subtitle":"FAIR Digital Object Search"
 };
 
 
