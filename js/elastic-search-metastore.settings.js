33ddcf5805aae2fa9c8ad37f3ac714db1bc2508b
Volker Hartmann <volker.hartmann@kit.edu>
2023-06-16 11:41:08 +0200

    Adapt search settings for MetaStore for docker.

--- js/elastic-search-metastore.settings.js
+++ js/elastic-search-metastore.settings.js
@@ -1,11 +1,5 @@
-//export let ajaxBaseUrl = "https://demo.datamanager.kit.edu:8443/metastore/api/v1/";
-export let ajaxBaseUrl = "http://localhost:8041/api/v1/";
+export let ajaxBaseUrl = "http://localhost:8040/api/v1/";
 export const keycloak = undefined;
-/*Keycloak({
-    url: 'https://gateway.datamanager.kit.edu:8443/',
-    realm: 'dem_testing',
-    clientId: 'kitdm-services'
-});*/
 
 export const showServiceUrl = false;
 export const page_size = 5;
