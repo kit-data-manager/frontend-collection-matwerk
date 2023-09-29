ed524d369dce9c297deacc740afb7dc4017a50d8
Thomas Jejkal <thomas.jejkal@kit.edu>
2023-09-29 11:12:09 +0200

    Renamed FDO Maker to FDO Builder, enabled Typed PID Maker backend access

--- settings/fdo-builder.settings.js
+++ settings/fdo-builder.settings.js
@@ -5,6 +5,6 @@ export let ajaxBaseUrl = "http://localhost:8090";
 //The app description used to customize the frontend, e.g., for a specific project with a custom title and subtitle.
 export const appDescription = {
     "app-logo":"./images/typed-pid-maker-logo.svg",
-    "app-title":"FAIR-DO Creator",
+    "app-title":"FAIR-DO Builder",
     "app-subtitle":"Interactively create your FAIR-DOs."
 };
