735269c0fcec47e74b1ba7e4cb0388fe279b99c7
Volker Hartmann <volker.hartmann@kit.edu>
2024-04-10 13:44:50 +0200

    Restore default settings

--- settings/metastore.settings.js
+++ settings/metastore.settings.js
@@ -1,5 +1,5 @@
 //The backend service URL for the MetaStore instance used by the frontend.
-export const ajaxBaseUrl = "http://metastore.docker:8040/metastore/api/v1/";
+export const ajaxBaseUrl = "http://localhost:8041/api/v1/";
 //export const ajaxBaseUrl = "https://demo.datamanager.kit.edu:8443/metastore/api/v1/";
 
 //The app description used to customize the frontend, e.g., for a specific project with a custom title and subtitle.
@@ -12,7 +12,7 @@ export const appDescription = {
 //Enable/disable the Elastic search functionality. The availability of the search depends on the configuration of
 //the configured MetaStore instance. If Elastic search is not configured for the underlying MetaStore, it should
 //also be disabled here.
-export const searchEnabled = true;
+export const searchEnabled = false;
 
 
 
